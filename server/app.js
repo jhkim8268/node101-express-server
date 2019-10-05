1bd3f84e6890adae521ba871dc654f2c0fb30856
jhkim8268@gmail.com <jhkim8268@gmail.com>
2019-10-05 13:54:10 -0700

    built express server

--- server/app.js
+++ server/app.js
@@ -1,10 +1,21 @@
 // import files and packages up here
+const express = require('express');
+const logger = require('morgan');
+const data = require('./data')
+// const fs = require('fs');
 
+const app = express();
+app.use(logger('combined'))
 
-// create your express server below
-var app;
+app.get('/', (req, res) => res.status(200).send(`<h1>Hello World</h1>`))
 
-// add your routes and middleware below
+app.get('/data', (req, res) => res.status(200).send(data))
 
-// finally export the express application
+// app.get('/data', (req, res) => 
+//   fs.readFile('./server/data.json', 'utf8', (err, data) => {
+//     if(err) throw err;
+//     res.status(200).send(data)
+//   })
+// )
+
 module.exports = app;
